9416f84c1d6ebf38569a1d23f4c5a5ec22521690
jkim83 <123273928+jkim83@users.noreply.github.com>
2024-04-21 01:37:48 -0400

    Update alg.py

--- alg.py
+++ alg.py
@@ -1,34 +1,39 @@
 import solitaire
 
 def simulate_game(board, steps=[], dead_ends=0, moves_made=0, consecutive_draws=0):
+    global prev_t_to_t_move  # Declare prev_t_to_t_move as a global variable
     print("Top card of waste:", board.piles[12][1] if board.piles[12] else "Empty")
     moveMade = False
 
-    # RULE 1: Move to pile.
-    for i in range(8):  # 
-        for j in range(8, 12):  # Check if any card can be moved from tableau to pile
-            if board.check_validity(i, j):
-                board.move(i, j)
-                print(i, j)
-                moveMade = True
-                return
-    
-    # RULE 2: Move between tableaus.
-    # note: remember previous btwn tableaus so you don't have infinite loop?
+    # RULE 1: tableau to pile.
     if not moveMade:
-        for i in range(8):  # Check if ANY movement within tableaus is valid
-            for j in range(8):  # Check if any card can be moved within the tableau
+        for i in range(8):
+            for j in range(8, 12):
+                print(i, j)
                 if board.check_validity(i, j):
                     board.move(i, j)
                     print(i, j)
                     moveMade = True
                     return
+    
+    # RULE 2: Move between tableaus.
+    if not moveMade:
+        for i in range(8):
+            for j in range(8):
+                # Check if the move is valid and not the same as the previous tableau-to-tableau move
+                if board.check_validity(i, j) and prev_t_to_t_move != (i, j) and prev_t_to_t_move != (j, i):
+                    board.move(i, j)
+                    print(i, j)
+                    prev_t_to_t_move = (i, j)  # Update prev_t_to_t_move with the current move
+                    print(prev_t_to_t_move)
+                    moveMade = True
+                    return
 
     # RULE 3: Move from waste to tableaus.
-    if not moveMade and board.piles[12]:  # Check if no move has been made and there are cards in the waste pile
-        for i in range(8):  # Iterate over tableaus
-            if board.check_validity(12, i):  # Check if moving from waste to tableau 'i' is valid
-                board.move(12, i)  # Move the card from waste to tableau 'i'
+    if not moveMade and board.piles[12]:
+        for i in range(8):
+            if board.check_validity(12, i):
+                board.move(12, i)
                 print('RULE #3')
                 print(12, i)
                 moveMade = True
@@ -42,6 +47,8 @@ def simulate_game(board, steps=[], dead_ends=0, moves_made=0, consecutive_draws=
     return
 
 def main():
+    global prev_t_to_t_move
+    prev_t_to_t_move = (-1, -1)
     board = solitaire.Solitaire()
 
     board.displayBoard()
